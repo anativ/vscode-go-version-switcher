9ef270e97a074e2d1248b8c506b185fac64e6793
anativ <alon.nativ@tomorrow.io>
2024-01-25 13:40:59 +0200

    first version 0.0.1

---
+++ README.md
@@ -0,0 +1,25 @@
+# VS Code Go Version Manager (GVM) Extension
+
+## Overview
+
+This Visual Studio Code extension allows users to seamlessly switch between different Go versions managed by GVM (Go Version Manager). It updates the `go.goroot` and `go.gopath` settings in VS Code based on the selected Go version from the gvm list.
+
+## Features
+
+* Easy integration with GVM.
+* Quick selection of Go versions directly from VS Code.
+* Automatic update of `go.goroot` and `go.gopath` workspace settings.
+
+## Usage
+
+1. Open the Command Palette (Ctrl+Shift+P or Cmd+Shift+P on macOS).
+2. Type and select Select Go Version from the list of commands.
+3. A dropdown will appear with the list of installed Go versions (as per gvm list).
+4. Select the desired Go version. The extension will automatically update the go.goroot and go.gopath settings for the workspace.
+
+## Contributing
+
+Contributions to the extension are welcome! Please feel free to fork the repository, make changes, and create a pull request.
+
+## Author
+Alon Nativ @anativ
---
+++ extension.js
@@ -0,0 +1,69 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const { exec } = require('child_process');
+const os = require('os');
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "go-version-switcher" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('go-version-switcher.run', function () {
+		   // Execute gvm list
+		   exec('gvm list', (err, stdout, stderr) => {
+            if (err) {
+                vscode.window.showErrorMessage('Error listing Go versions');
+                return;
+            }
+
+            let goVersions = parseGvmListOutput(stdout);
+            vscode.window.showQuickPick(goVersions).then(selectedVersion => {
+                if (selectedVersion) {
+					vscode.window.showInformationMessage('Selected Version: ' + selectedVersion);
+					updateGoSettings(selectedVersion);
+                }
+            });
+        });
+		// Display a message box to the user
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+
+
+function updateGoSettings(version) {
+	const homeDir = os.homedir();
+    const gorootPath = `${homeDir}/.gvm/gos/${version}`;
+    const gopathPath = `${homeDir}/.gvm/pkgsets/${version}/global`;
+
+    let config = vscode.workspace.getConfiguration('go');
+    config.update('goroot', gorootPath, vscode.ConfigurationTarget.Workspace);
+    config.update('gopath', gopathPath, vscode.ConfigurationTarget.Workspace);
+}
+
+function parseGvmListOutput(output) {
+    // Split the output into lines and filter out only the Go version lines
+    return output.split('\n')
+                 .map(line => line.trim())
+                 .filter(line => line.startsWith('go') && line.match(/^go\d+\.\d+/))
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
